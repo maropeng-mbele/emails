978c1ad2bb2ed660b4c535e9f5155055b1785b23
maropeng-mbele <maropeng.mbele@gmail.com>
2024-11-22 06:54:26 +0200

    change

--- app.py
+++ app.py
@@ -131,12 +131,12 @@ class EmailComposerAndSender:
                 # Get client config from Streamlit secrets
                 client_config = {
                     "installed": {
-                        "client_id": st.secrets["client_id"],
-                        "project_id": st.secrets["project_id"],
+                        "client_id": st.secrets.google["client_id"],
+                        "project_id": st.secrets.google["project_id"],
                         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
                         "token_uri": "https://oauth2.googleapis.com/token",
                         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                        "client_secret": st.secrets["client_secret"],
+                        "client_secret": st.secrets.google["client_secret"],
                         "redirect_uris": ["http://localhost"]
                     }
                 }
